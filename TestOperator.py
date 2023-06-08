Fix gradient bug

TestOperator.py
@@ -41,7 +41,7 @@ class TestOperator(bpy.types.Operator):
             centroid = np.average([obj.matrix_world @ v.co for v in tri.verts], axis=0)
             grad_magnitudes.append(np.linalg.norm(grad))
             gradient_normalized = grad / np.linalg.norm(grad)
-            visualdebug.create_dir_vector(f'dir{tri.index}', centroid, gradient_normalized, length=self.length)
+            visualdebug.create_dir_vector(f'dir{tri.index}', centroid, grad, length=self.length)
 
         # set 'height' attribute to z coord of each vertex
         set_vertex_attrib(obj, 'fun', np.array([self.fun(v.co) for v in bm.verts]))

meshutil.py
@@ -1,15 +1,19 @@
 import bmesh
+import numpy as np
 import scipy.sparse as sp
 from bmesh.types import BMFace, BMEdge, BMVert
 from mathutils import Vector
 
 from bpyutil import *
+from visualdebug import create_dir_vector, create_edge_vector
+
 
 def mesh_from_object(object) -> BMesh:
     mesh = object.data
     bm = bmesh.new()
     bm.from_mesh(mesh)
     return bm
+
 
 def triangulate_object(obj):
     """
@@ -33,6 +37,7 @@ def triangulate_object(obj):
     bm.to_mesh(me)
     bm.free()
 
+
 def neighbors(v: BMVert, only_boundaries=False) -> list[BMVert]:
     neighbors = []
     for e in v.link_edges:
@@ -49,6 +54,7 @@ def neighbors(v: BMVert, only_boundaries=False) -> list[BMVert]:
 
     return neighbors
 
+
 def centroid(ps: list[Vector]):
     p_sum = Vector((0, 0, 0))
     for p in ps:
@@ -56,9 +62,11 @@ def centroid(ps: list[Vector]):
 
     return p_sum / len(ps)
 
+
 def is_boundary_edge(e: BMEdge) -> bool:
     # edge is a boundary loop when it only has one adjacent face
     return len(e.link_faces) == 1
+
 
 def compute_genus(bm: BMesh, n_boundaries: int) -> float:
     n_verts = len(list(bm.verts))
@@ -66,6 +74,7 @@ def compute_genus(bm: BMesh, n_boundaries: int) -> float:
     n_faces = len(list(bm.faces))
 
     return 1 - (n_verts - n_edges + n_faces + n_boundaries) / 2
+
 
 def compute_boundary_loops(bm: BMesh, select_edges=False) -> int:
     # find all edges that are boundaries
@@ -113,6 +122,7 @@ def compute_boundary_loops(bm: BMesh, select_edges=False) -> int:
 
     return n_loops
 
+
 def compute_mesh_volume(bm: BMesh) -> float:
     total_volume = 0
     for face in bm.faces:
@@ -123,6 +133,7 @@ def compute_mesh_volume(bm: BMesh) -> float:
         total_volume += tetra_volume
 
     return total_volume
+
 
 def compute_connected_components(bm: BMesh) -> int:
     n_components = 0
@@ -152,6 +163,7 @@ def compute_connected_components(bm: BMesh) -> int:
 
     return n_components
 
+
 def compute_laplace_coords(bm: BMesh) -> dict[int, Vector]:
     # precompute laplace coordinates of mesh
     laplace_coords = {}
@@ -168,6 +180,7 @@ def compute_laplace_coords(bm: BMesh) -> dict[int, Vector]:
         laplace_coords[v.index] = laplace_coord
 
     return laplace_coords
+
 
 def mesh_laplacian(mesh: BMesh) -> np.ndarray:
     n = len(mesh.verts)
@@ -186,6 +199,8 @@ def mesh_laplacian(mesh: BMesh) -> np.ndarray:
     L = sp.eye(n) - sp.linalg.inv(D.tocsc()) @ A
     return L.tocsc()
 
+
+# TODO: Replace with triange.get_area()
 def triangle_area(triangle: BMFace) -> float:
     """Calculates the mass matrix for a single triangle:
     """
@@ -198,6 +213,7 @@ def triangle_area(triangle: BMFace) -> float:
     sinTheta = np.sqrt(1 - cosTheta ** 2)
     area = 0.5 * AB.length * AC.length * sinTheta
     return area
+
 
 def compute_mass_matrix(mesh: BMesh, return_sparse=True) -> np.ndarray:
     """
@@ -219,12 +235,16 @@ def compute_mass_matrix(mesh: BMesh, return_sparse=True) -> np.ndarray:
         mass = sp.csr_matrix(mass)
     return mass
 
+
 def bmedge_vector(edge: BMEdge) -> Vector:
     v1, v2 = edge.verts
+    create_dir_vector("Debug", np.array(v1.co), np.array(v2.co - v1.co), length=1)
     return v2.co - v1.co
+
 
 def opposite_edge(tri: BMFace, v: BMVert) -> BMEdge:
     return [e for e in tri.edges if e not in v.link_edges][0]
+
 
 def compute_gradient_matrix(bm: BMesh, return_sparse=True) -> np.ndarray:
     """
@@ -244,9 +264,12 @@ def compute_gradient_matrix(bm: BMesh, return_sparse=True) -> np.ndarray:
 
         local_gradient_matrix = np.zeros((3, n))
 
-        for v in face.verts:
-            opp_edge = opposite_edge(face, v)
-            opp_edge = bmedge_vector(opp_edge)
+        face_verts = list(face.verts)
+        for v_ix, v in enumerate(face_verts):
+            # Get subsequent vertices
+            successor = face_verts[(v_ix + 1) % 3].co
+            suc_successor = face_verts[(v_ix + 2) % 3].co
+            opp_edge = suc_successor - successor
 
             grad = constant * normal.cross(opp_edge)
             grad = np.array(grad)
@@ -258,6 +281,7 @@ def compute_gradient_matrix(bm: BMesh, return_sparse=True) -> np.ndarray:
         global_gradient_matrix = sp.csr_matrix(global_gradient_matrix)
     return global_gradient_matrix
 
+
 def compute_cotangent_matrix(bm: BMesh):
     """
     Computes cotangent matrix of a mesh.
@@ -265,6 +289,7 @@ def compute_cotangent_matrix(bm: BMesh):
     gradient_matrix = compute_gradient_matrix(bm)
     mass_matrix = compute_mass_matrix(bm)
     return gradient_matrix.T @ mass_matrix @ gradient_matrix
+
 
 def compute_deformation_matrices(bm: BMesh) -> (sp.csr_matrix, sp.csr_matrix):
     """